5564009d50dd014da1db655a7a6c91807d9954a6
Jack Schaefer <jackschaefer64@gmail.com>
2025-10-30 13:31:52 -0400

    Update Goblin.java

--- Assignment 7 /Goblin.java
+++ Assignment 7 /Goblin.java
@@ -4,8 +4,11 @@ public class Goblin extends Enemy{
     {
         super(h, d);
     }
+    //Inherits damage and health from the main Enemy class
     void attack()
     {
         System.out.println("The goblin gobbles!");
     }
+
+    //This is a goblin enemy that extends from the enemy class. It has it's own attack message
 }
