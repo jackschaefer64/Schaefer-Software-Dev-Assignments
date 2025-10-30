7d9a9757825f9a2b1b9b9152c6c80f96e3784b6f
Jack Schaefer <jackschaefer64@gmail.com>
2025-10-30 13:28:13 -0400

    Create Goblin.java

---
+++ Assignment 7 /Goblin.java
@@ -0,0 +1,11 @@
+public class Goblin extends Enemy{
+
+    public Goblin(int h, int d)
+    {
+        super(h, d);
+    }
+    void attack()
+    {
+        System.out.println("The goblin gobbles!");
+    }
+}
