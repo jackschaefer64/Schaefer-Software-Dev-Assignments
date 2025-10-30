ee2b7638efd59b8ccb688d960e6912895395e195
Jack Schaefer <jackschaefer64@gmail.com>
2025-10-30 13:32:52 -0400

    Update Enemy.java

--- Assignment 7 /Enemy.java
+++ Assignment 7 /Enemy.java
@@ -11,4 +11,6 @@ public class Enemy {
     {
         System.out.println("The enemy attacks!");
     }
+    //This is the main enemy class. It has health and damage variables that it gives to any other classes that inherit it.
+    //A basic enemy has a basic attack message
 }
